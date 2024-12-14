e91bc76edb0669e315640b098a499e4d6dce7c91
artvinn <artjomvinnikov@gmail.com>
2024-12-14 15:42:28 +0200

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -510,6 +510,10 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'eslint-lsp',
+        'eslint_d',
+        'prettierd',
+        'prettier'
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
