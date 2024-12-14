68b8e7d6abc79facc7009cd668f7a3e9b2951271
Artjom Vinnikov <artjom.vinnikov@depowise.com>
2024-12-14 13:34:33 +0200

    updates

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -28,7 +28,7 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-      vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_enable_italic = false
       vim.g.sonokai_style = 'maia'
       vim.g.sonokai_better_performance = 1
       vim.cmd.colorscheme 'sonokai'
