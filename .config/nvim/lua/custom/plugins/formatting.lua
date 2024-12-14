d27a15b9d9b1a9d620fe0c5a952283cc74464545
Artjom Vinnikov <artjom.vinnikov@depowise.com>
2024-12-14 17:36:10 +0200

    disable prettier

--- .config/nvim/lua/custom/plugins/formatting.lua
+++ .config/nvim/lua/custom/plugins/formatting.lua
@@ -19,16 +19,16 @@ return { -- Autoformat
   opts = {
     -- Define your formatters
     formatters_by_ft = {
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      svelte = { 'prettierd', 'prettier', stop_after_first = true },
-      css = { 'prettierd', 'prettier', stop_after_first = true },
-      html = { 'prettierd', 'prettier', stop_after_first = true },
-      json = { 'prettierd', 'prettier', stop_after_first = true },
-      yaml = { 'prettierd', 'prettier', stop_after_first = true },
-      markdown = { 'prettierd', 'prettier', stop_after_first = true },
+      -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      -- javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      -- typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      -- typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      -- svelte = { 'prettierd', 'prettier', stop_after_first = true },
+      -- css = { 'prettierd', 'prettier', stop_after_first = true },
+      -- html = { 'prettierd', 'prettier', stop_after_first = true },
+      -- json = { 'prettierd', 'prettier', stop_after_first = true },
+      -- yaml = { 'prettierd', 'prettier', stop_after_first = true },
+      -- markdown = { 'prettierd', 'prettier', stop_after_first = true },
       lua = { 'stylua' },
     },
     -- Set default options
