acae7fb0a80532d85be322fc5197c2a12dba10ca
Artjom Vinnikov <artjom.vinnikov@depowise.com>
2026-01-21 16:26:20 +0200

    updates

--- .config/nvim/lua/custom/plugins/arrow.lua
+++ .config/nvim/lua/custom/plugins/arrow.lua
@@ -7,7 +7,7 @@ return {
   },
   opts = {
     show_icons = true,
-    leader_key = '<leader>l', -- Recommended to be a single key
-    buffer_leader_key = '<leader>;', -- Per Buffer Mappings
+    leader_key = ',', -- Recommended to be a single key
+    buffer_leader_key = '<leader>,', -- Per Buffer Mappings
   }
 }
